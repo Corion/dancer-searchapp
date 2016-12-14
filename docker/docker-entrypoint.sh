a91d2a2673bb19081b0ce4d9c74995a4af93045f
Max Maischein <corion@corion.net>
2016-12-14 19:05:59 +0100

    Put dependencies below vendor/
    This is just for giggles as we could put them anywhere because
    we are running in a container anyway

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'dancer-searchapp' ]; then
     shift
     echo "$@"
-    exec plackup -s Twiggy --port 8080 -a bin/app.pl "$@"
+    exec plackup -s Twiggy -Ivendor/ --port 8080 -a bin/app.pl "$@"
 fi
 
 exec "$@"
